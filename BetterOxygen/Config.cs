2e1dc2faef6013dd5e35bd661cd87cf1f7537959
TonyGamer <tomblackaz@gmail.com>
2024-04-12 15:26:36 -0700

    Changed default refill rate back to 0.1

--- BetterOxygen/Config.cs
+++ BetterOxygen/Config.cs
@@ -17,6 +17,6 @@ public class Config
         maximumOxygen = cfg.Bind<float>("Oxygen Levels", "Maximum Oxygen (Minutes):", 12f, "Sets the amount of oxygen you have in minutes, restart required to take effect. Unmodded game is 8 minutes and 20 seconds or 8.33");
         infiniteOxygen = cfg.Bind("Extra Options", "Do you want infinite oxygen?", defaultValue: false, "Enabling this makes your oxygen last forever");
         refillOxygen = cfg.Bind("Extra Options", "Refill oxygen inside the diving bell?", defaultValue: false, "Enabling this makes your oxygen refill over time when standing in the diving bell. (A bit more fair than infinite O2)");
-        oxygenRefillRate = cfg.Bind("Extra Options", "Oxygen Refill Rate (seconds/second):", defaultValue: 0f, "Controls the rate at which oxygen refills while standing inside the diving bell, in seconds of O2 gained per second inside the bell");
+        oxygenRefillRate = cfg.Bind("Extra Options", "Oxygen Refill Rate (seconds/second):", defaultValue: 0.1f, "Controls the rate at which oxygen refills while standing inside the diving bell, in seconds of O2 gained per second inside the bell");
     }
 }
